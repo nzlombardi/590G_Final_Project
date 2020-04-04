cbb6f65071992080c7897454d051b5e85853ba16
Eric <erictang@umass.edu>
2020-04-03 21:29:52 -0400

    Launcher v.1

---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour {
+
+	public GameObject manager;
+	
+	void OnCollisionEnter(Collision target){
+		if(target.gameObject.name == "Rocket(Clone)"){
+			Destroy(gameObject);
+		}
+	}
+}
